4c5325535b6ac157dd159c8787693229502afd42
Rafal Szczotka <rafalszczotka@wp.pl>
2020-10-14 20:07:59 +0100

    Project initialized

---
+++ src/main/java/com/referenceit/ReferenceitApplication.java
@@ -0,0 +1,13 @@
+package com.referenceit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/referenceit/ReferenceitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.referenceit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
